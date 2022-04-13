c109302e0e40c00c03e9d8415e484f8d2d03b2a0
landonraph <landoncraph@gmail.com>
2022-04-12 21:46:34 -0500

    First Commit

---
+++ idle-coder-app/src/app/click-button/click-button.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-click-button',
+  templateUrl: './click-button.component.html',
+  styleUrls: ['./click-button.component.css']
+})
+export class ClickButtonComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
